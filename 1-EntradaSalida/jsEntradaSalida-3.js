4acd51fc07ff78cd4b4f99b808645c5ec0d4efde
Gdoval <charly_sxxi@hotmail.com>
2017-06-26 12:06:04 -0300

    Ejercicio 3
    Explicación del document.algo

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -3,7 +3,9 @@ y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
     var nombre;
-	nombre = document.getElementById("elNombre").value;
+	nombre = document.getElementById("elNombre").value; 
+    /* el document.algo inspecciona el codigo html
+    buscando el string que se le pasa como argumento. El .value trae el valor asociado o guardado en el codigo html y puede ser alojado en una variable, previamente declarada */
     alert (nombre);
 
 }
