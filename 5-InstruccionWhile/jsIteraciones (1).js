47504f0e0d65c3d91b8bc261931cd1d8ba5b1387
GDoval <charly_sxxi@hotmail.com>
2017-07-05 11:37:43 -0300

    Ejercicios 1 y 2 del modulo While

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,11 @@
 function Mostrar()
 {
-	alert('iteración while');
-
+	var numero;
+	numero = 1;
+	while (numero < 11)
+	{
+		alert(numero);
+		numero += 1;
+	}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var num;
+	num = 10;
+	while (num > 0)
+	{
+		alert(num);
+		num -= 1;
+	}
 
 
 }//FIN DE LA FUNCIÓN
