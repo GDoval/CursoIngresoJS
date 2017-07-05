b0494a305f5d520358d2fc3477bbf9badc889a98
GDoval <charly_sxxi@hotmail.com>
2017-07-05 09:01:27 -0300

    Resolución del primer TP (FerreteFacturacion)

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,40 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	
 function Sumar () 
 {
-	
+
+	preciouno = document.getElementById("PrecioUno").value;
+	preciodos = document.getElementById("PrecioDos").value;
+	preciotres = document.getElementById('PrecioTres').value;
+	var suma;
+	suma = parseInt(preciouno) + parseInt(preciodos) + parseInt(preciotres);
+	alert("La suma de los precios es: " + suma);
 }
+
 function Promedio () 
 {
-	
+	preciouno = document.getElementById("PrecioUno").value;
+	preciodos = document.getElementById("PrecioDos").value;
+	preciotres = document.getElementById('PrecioTres').value;
+	var promedio;
+	promedio = (parseInt(preciouno) + parseInt(preciodos) + parseInt(preciotres)) / 3 ;
+	alert ("El promedio de los precios es: " + promedio); 
 }
+
 function PrecioFinal () 
 {
-	
-}
+	preciouno = document.getElementById("PrecioUno").value;
+	preciodos = document.getElementById("PrecioDos").value;
+	preciotres = document.getElementById('PrecioTres').value;
+	var preciofinal;
+	var resultado;
+	resultado = parseInt(preciouno) + parseInt(preciodos) + parseInt(preciotres);
+	preciofinal = resultado + (resultado * 0.21);
+	alert ("El precio final mas IVA es : " + preciofinal);
+
+} 
