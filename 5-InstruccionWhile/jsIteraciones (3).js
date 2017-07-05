eb3d69fc6955892148cfdf52c1be0859a888e9c4
GDoval <charly_sxxi@hotmail.com>
2017-07-05 12:09:03 -0300

    Ejercicios del While bien hechos

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave;
+	clave = prompt("Ingrese la clave ");
+	while (clave != "utn750")
+	{
+		clave = prompt("Ingrese nuevamente la clave ");
+	}
+	
+	alert ("Bienvenido");
 
 
 }//FIN DE LA FUNCIÓN
