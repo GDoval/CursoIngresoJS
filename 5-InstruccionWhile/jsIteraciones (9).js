34dd3390ec4287e4bb3593bea43a8182dfa545f9
GDoval <charly_sxxi@hotmail.com>
2017-07-10 11:48:57 -0300

    Ejercicio 9 resuelto a los bifes

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,42 @@
 function Mostrar()
 {
+	var num;
+	var respuesta;
+	var max;
+	var min;
+	var primera;
+	primera = true;
+	respuesta = "si";
+	while (respuesta == "si")
+		{
+			num = prompt("Ingrese un numero");
+			if (primera == true) //Se hace para validar si es la primera vez que se corre el programa. Asi, el mismo numero es el mayot y el menor (si es que el usuario no sigue ingresando numeros)
+			{
+				max = num;
+				min = num;
+				primera = false;
+			}
+			else
+			{
+				if (num < min)
+				{
+					min = num;
+									}
+				else
+				{
+					if (num > max)
+					{
+						max = num;
+						
+					}
+				}
+			}
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
-
-	while(respuesta!='no')
-	{
+			respuesta = prompt("Quiere seguir cargando numeros? ");
+			
+		}
 		
-	
-	}
+		document.getElementById("maximo").value = max;
+		document.getElementById('minimo').value = min;
 
-
-
-
-}//FIN DE LA FUNCIÓN
+}
